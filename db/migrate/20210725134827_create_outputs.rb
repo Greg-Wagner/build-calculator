221850ac84591c05fb67757fbd2d5f2895002f70
Greg Wagner <gregwagnerdev@gmail.com>
2021-08-17 12:18:36 -0400

    beginning changes after meeting with client

--- db/migrate/20210725134827_create_outputs.rb
+++ db/migrate/20210725134827_create_outputs.rb
@@ -4,20 +4,26 @@ class CreateOutputs < ActiveRecord::Migration[6.0]
 
       t.string :client_name
       t.float :sqft
-      t.float :lot_size
       t.string :location1
       t.string :location2
       t.string :location3
       t.float :location1_min
       t.float :location2_min
       t.float :location3_min
+      t.float :location1_min_override
+      t.float :location2_min_override
+      t.float :location3_min_override
       t.float :location1_max
       t.float :location2_max
       t.float :location3_max
+      t.float :location1_max_override
+      t.float :location2_max_override
+      t.float :location3_max_override
       t.float :total_range_min
       t.float :total_range_max
       t.float :build_estimate_min
       t.float :build_estimate_max
+      t.text :hcl
       t.float :total_cost_min
       t.float :total_cost_max
 
